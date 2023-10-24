990c104a1ae684684360159256bb8404a5b7d133
MANIRAJ9 <148906232+MANIRAJ9@users.noreply.github.com>
2023-10-25 00:35:22 +0530

    Create apps.js

---
+++ apps.js
@@ -0,0 +1,85 @@
+// Import dependencies
+const express = require('express');
+const mongoose = require('mongoose');
+
+// Set up Express app
+const app = express();
+app.use(express.json());
+
+// Connect to MongoDB
+mongoose.connect('mongodb://localhost:27017/mydatabase', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => {
+  console.log('Connected to MongoDB');
+});
+
+// Create User schema
+const userSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+});
+
+// Create User model
+const User = mongoose.model('User', userSchema);
+
+// API endpoints
+app.get('/users', async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+app.get('/users/:id', async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    res.json(user);
+  } catch (err) {
+    res.status(404).json({ message: 'User not found' });
+  }
+});
+
+app.post('/users', async (req, res) => {
+  const user = new User({
+    name: req.body.name,
+    email: req.body.email,
+  });
+
+  try {
+    const newUser = await user.save();
+    res.status(201).json(newUser);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+app.put('/users/:id', async (req, res) => {
+  try {
+    const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    res.json(user);
+  } catch (err) {
+    res.status(404).json({ message: 'User not found' });
+  }
+});
+
+app.delete('/users/:id', async (req, res) => {
+  try {
+    await User.findByIdAndDelete(req.params.id);
+    res.json({ message: 'User deleted' });
+  } catch (err) {
+    res.status(404).json({ message: 'User not found' });
+  }
+});
+
+// Start the server
+app.listen(3000, () => {
+  console.log('Server started on port 3000');
+});
